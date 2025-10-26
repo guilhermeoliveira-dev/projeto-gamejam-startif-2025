bdd4c6ccbae99f608420a9bd127ffdf5470ab3ca
Guilherme de Almeida Oliveira <guilhermeovergame@gmail.com>
2025-10-26 16:00:59 -0300

    Primeira Fase Finalizada

--- scripts/fase_1.gd
+++ scripts/fase_1.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 #@onready var tela_dialogo: CanvasLayer = %Dialogo
 
+@onready var final_fase: Area2D = %FinalFase
 
 
-#func _ready():
-	#
-	#
-	#
-	#
+func _ready():
+	
+	final_fase.body_entered.connect(encerrar_cena)
+
+
+func encerrar_cena(_body):
+	
+	#print("oi")
+	if _body.is_in_group("original_player"):
+	#musica_player.stream.
+		SceneController.changeSceneTo("res://cenas/cutscenes/cutscene_2.tscn")
